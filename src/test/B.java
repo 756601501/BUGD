505e76e8d5af07d1ead6cd7d4c51393d6cab89c3
chuandong_chen <chuandong_chen@vomoho.com>
2016-06-28 11:08:12 +0800

    ccd002分支上增加B文件

---
+++ src/test/B.java
@@ -0,0 +1,14 @@
+package test;
+
+public class B {
+	private String bName;
+
+	public String getbName() {
+		return bName;
+	}
+
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+	
+}
